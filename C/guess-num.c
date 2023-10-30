7784bbb10c687f292299110b43b5cff9f7089580
SysterrorTheERR <sys.terror@outlook.com>
2023-10-30 16:20:16 +0800

    优化 guess-num.c

--- C/guess-num.c
+++ C/guess-num.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 int main()
-{
+{ 
     int ans = 92;
     int guess;
-    while (1)
+    int count = 0;
+    while (count == 0 || guess != ans)
     {
-        printf("键入你猜测的数字：");
-        scanf("%d", &guess);
-        if (guess == ans)
-        {
-            printf("你猜对了！\n");
-            break;
-        }
         if (guess > ans)
         {
             printf("太大！\n");
         }
-        if (guess < ans)
+        else
         {
             printf("太小！\n");
         }
+        printf("键入你猜测的数字：");
+        scanf("%d", &guess);
+        count = count + 1;
     }
+    printf("猜得分毫不差！(%d 次)", count);
     return 0;
 }
