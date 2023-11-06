9cdf90310176b9bf4562d324da7d55e92f3d6656
SysterrorTheERR <sys.terror@outlook.com>
2023-11-06 17:03:30 +0800

    Add files via upload

---
+++ C/cal-avg.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int num;
+    int sum = 0;
+    int count = 0;
+    float avg;
+    printf("键入要求均值的若干个数字（q 代表退出）：\n");
+    scanf("%d", &num);
+    while (num != 0)
+    {
+        sum = sum + num;
+        count = count + 1;
+        scanf("%d", &num);
+    }
+    avg = (float) sum / count;
+    printf("均值为 %f。\n", avg);
+    return 0;
+}
---
+++ C/sort-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int a, b, c, t;
+    printf("键入你想要排序的三个数字；");
+    scanf("%d%d%d", &a, &b, &c);
+    printf("Before: %d %d %d\n", a, b, c);
+    /*一些处理，使按小中大排序*/
+    if (b < a)
+    {
+        t = b; b = a; a = t;
+    }
+    if (c < a)
+    {
+        t = c; c = a; a = t;
+    }
+    if (c < b)
+    {
+        t = c; c = b; b = t;
+    }
+    printf("After: %d %d %d\n", a, b, c);
+    return 0;
+}
