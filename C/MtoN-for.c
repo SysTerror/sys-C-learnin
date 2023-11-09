e59ee245cccc02624557e146473aa943741666ec
SysterrorTheERR <sys.terror@outlook.com>
2023-11-09 17:27:07 +0800

    打印连续整数 M 到 N，两种循环方式

---
+++ C/MtoN-for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int count, M, N;
+    printf("键入你想印出的 M 到 N 的 M：");
+    scanf("%d", &M);
+    printf("现在键入 M 到 N 的 N：");
+    scanf("%d", &N);
+    for (count = M; count <= N; count++)
+    {
+        printf("%d\n", count);
+    }
+    return 0;
+}
