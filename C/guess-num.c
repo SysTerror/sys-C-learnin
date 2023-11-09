a587b7952e3fb0b05dd9cec9169ca299114703d3
SysterrorTheERR <sys.terror@outlook.com>
2023-11-09 11:36:22 +0800

    Add files via upload

--- C/guess-num.c
+++ C/guess-num.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 int main()
-{ 
+{
     int ans = 92;
     int guess;
     int count = 0;
-    while (count == 0 || guess != ans)
+    // 方法 1：使用 do while 述句
+    do
     {
         printf("键入你猜测的数字：");
         scanf("%d", &guess);
-        count = count + 1;
+        count = count + 1; // 计数
+        /* 判断猜测数字与谜底数字的大小差别 */
         if (guess > ans)
         {
             printf("太大！\n");
@@ -17,7 +19,13 @@ int main()
         {
             printf("太小！\n");
         }
+    } while (guess != ans);
+    /* 方法 2：使用 while 述句
+    while (count == 0 || guess != ans)
+    {
+        // 内部程式片段同方法 1
     }
+    */
     printf("猜得分毫不差！(%d 次)", count);
     return 0;
 }
