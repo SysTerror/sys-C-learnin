72971b3dc00cf4ae39f889a8793743395b42c188
SysterrorTheERR <sys.terror@outlook.com>
2024-02-29 00:54:10 +0000

    指标与字串

--- c/函式实现变数递增（使用指标）.c
+++ c/函式实现变数递增（使用指标）.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-void selfAdd (int *nAddr) { // 使传入地址的变数值自增 1
+void selfAdd (int *nAddr) { // 使传入地址的变数的值递增 1
     *nAddr = *nAddr + 1; // * 间接运算子，透过位址找到对应变数
+    // *nAddr++; 无法使 n 递增，相当于 *nAddr; nAddr++;
 }
 
 int main () {
---
+++ c/字串字面常数.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    char strA[] = "A_Pi";
+        // == {'A', '_', 'P', 'i', '\0'}，"\0"是字串结尾标识符
+    char *strB = "A_Pi";
+        // 字串字面常数，存在专门分配的位址，只读不写，可隐性转型成字元指标
+    strA[0] = 'B'; // 可以直接修改某一个字元
+    // *strB = 'C'; 编译错误，尝试写一处只读位址
+    while (*strB != '\0') {
+        printf("%c", *strB++);
+    }
+    printf("\n");
+    return 0;
+}
